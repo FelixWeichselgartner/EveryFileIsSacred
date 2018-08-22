7f5610acbd7cdf539e835da4e613212e8520d7dd
Felix Wei <felix.weichselgartner@t-online.de>
2018-08-22 19:34:37 +0200

    added comments for all functions

--- exchange_in_csv/exchange_in_csv.py
+++ exchange_in_csv/exchange_in_csv.py
@@ -8,13 +8,32 @@
 
 import fileinput, string, sys
 
-def file_lenght(fname): #works
+def file_lenght(fname):
+    """evaluates the amount of lines the file has
+    
+    Arguments:
+        fname {string} -- name of the translation-list
+    
+    Returns:
+        [int] -- number of line the file has
+    """
+
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
 
-def fill_list(Matrix, amount, filename): #works
+def fill_list(Matrix, amount, filename):
+    """fills the Matrix with the translation-list
+        Matrix[n][0] is the old string
+        Matrix[n][1] is the new string
+    
+    Arguments:
+        Matrix {list} -- Matrix that contains the old string and the new string
+        amount {integer} -- the amount of old and new string that are saved in the Matrix
+        filename {string} -- name of the translation-list
+    """
+
     for x in range(amount):
         for y in range(2):
             Matrix[x][y] = ''
@@ -34,12 +53,30 @@ def fill_list(Matrix, amount, filename): #works
                 Matrix[k][flag] += c
 
 def replace(Matrix, exchange_file, amount):
+    """searches the exchange file for the old string and replaces them with new one
+    
+    Arguments:
+        Matrix {list} -- Matrix that contains the old string and the new string
+        exchange_file {string} -- name of the file where the old string are searched and replaced
+        amount {integer} -- the amount of old and new string that are saved in the Matrix
+    """
+
     for v in range(amount):
         for line in fileinput.input(exchange_file, inplace=1):
             line = line.replace(Matrix[v][0], Matrix[v][1])
             sys.stdout.write(line)
 
 def main():
+    """
+    * This is a skript which requires a translation list.
+    * The translation list should be a .csv-file and have each translation per line.
+    * In the list there should be old string and new string divided by a ;.
+    * example:
+    *          old string; new string\n
+    * The skript read all the translation in a Matrix.
+    * It then searches the exchange_file for the old strings and replaces them with the new ones
+    """
+
     filename = input('Wie ist der Name der Übersetzungsliste?: ')
     read = open(filename,'r')
     amount = file_lenght(filename)
