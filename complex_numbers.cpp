5595aa844f190157672f101f35989fad54d6d7b7
FelixWeichselgartner <f.wei3012@gmail.com>
2018-11-29 09:43:37 +0100

    added division and conjugate

--- complex_numbers.cpp
+++ complex_numbers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 class Complex {
@@ -25,12 +26,12 @@ class Complex {
                 ;
             }
         }
-        calculate() {
+        void calculate() {
             const double pi = 3.14159265358979323846;
             a = sqrt(vreal * vreal + vimag * vimag);
             phi = 180 / pi * atan2(vimag, vreal);
         }
-        calculate2() {
+        void calculate2() {
             const double pi = 3.14159265358979323846;
             double phi_radian = phi / 180 * pi;
             vreal = a * cos(phi_radian);
@@ -69,13 +70,20 @@ class Complex {
             return phi;
         }
         void printNumber() {
-            cout << "Real part of " << name << " = " << vreal << endl;
-            cout << "Imag part of " << name << " = " << vimag << endl;
-            cout << "Amplitude of " << name << " = " << a << endl;
-            cout << "Phase of " << name << " " << phi << endl;
+            cout << name << ": " << vreal << " + j" << vimag << endl;
+            cout << a << "*exp(j * " << phi << ")" << endl;
         }
 
+        /*
+        Complex conjugate(Complex res) { 
+            res.vreal = vreal; res.vimag = (-1) * vimag;
+            return res; 
+        }
+        */
+
         //Overload + operator to add to complex numbers
+        //vreal & vimag is the first number
+        //obj is the second number
         Complex operator + (Complex const &obj) { 
             Complex res; 
             res.vreal = vreal + obj.vreal; 
@@ -99,7 +107,24 @@ class Complex {
             res.calculate2(); 
             return res; 
         }
+        //dividing objects
+        Complex operator / (Complex const &obj) { 
+            Complex res;
+            if (obj.vreal == 0 && obj.vimag == 0) {
+                res.vreal = 0; res.vimag = 0;
+            }
+            res.vreal = (vreal * obj.vreal + vimag * obj.vimag)/(obj.vreal * obj.vreal + obj.vimag * obj.vimag);
+            res.vimag = (obj.vimag * vreal - vimag * obj.vreal)/(obj.vreal * obj.vreal + obj.vimag * obj.vimag);
+            res.calculate(); 
+            return res; 
+        }
 };
+
+Complex conjugate(Complex n) {
+    Complex res; 
+    res.setR(n.getReal()); res.setI((-1) * n.getImaginary());
+    return res; 
+}
 
 void declare_complex() {
     string name; char form;
@@ -132,11 +157,12 @@ void declare_complex() {
 
 void overloading_test() {
     Complex number1 = Complex("number1", 1, 1, 'c');
-    Complex number2 = Complex("number2", 1, 1, 'c');
+    Complex number2 = Complex("number2", 2, 2, 'c');
     Complex number3 = Complex("number3", 0, 0, 'c');
     Complex number4 = Complex("number4", 0, 0, 'c');
     Complex number5 = Complex("number5", 0, 0, 'c');
     Complex number6 = Complex("number6", 0, 0, 'c');
+    Complex number7 = Complex("number7", 0, 0, 'c');
     cout << "number1: " << endl;
     number1.printNumber();
     cout << endl << "number2: " << endl;
@@ -150,11 +176,17 @@ void overloading_test() {
     cout << endl << "multiplying both numbers: " << endl;
     number5 = number1 * number2;
     number5.printNumber();
+    cout << endl << "dividing first number by second numbers: " << endl;
+    number6 = number1 / number2;
+    number6.printNumber();
+    cout << endl << "conjugate complex number of first number: " << endl;
+    number7 = conjugate(number1);
+    number7.printNumber();
     return;
 }
 
 int main() {
-    declare_complex();
+    //declare_complex();
     overloading_test();
     return 0;
 }
