a2b129f762218a13c075e8bc14b53f45f74d7795
Felix Wei <felix.weichselgartner@t-online.de>
2018-12-23 20:57:07 +0100

    program to calculate prime numbers

---
+++ primeNumber.cpp
@@ -0,0 +1,81 @@
+/*
+**************************************************************
+*  Copyright (c) 2018 Felix Weichselgartner
+*  all rights reserved
+**************************************************************
+*  Date of creating this file: 23.12.2018
+*  Last changes: 23.12.2018
+*  creator: Felix Weichselgartner
+*  purpose: trying out a selfmade
+*           prime number calculating algorithm
+**************************************************************
+*  original upload to:
+*  https://github.com/FelixWeichselgartner/EveryFileIsSacred
+**************************************************************
+*  Calculates the amount of primes numbers you tell it to
+**************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+#include <ctime>
+#define AmountOfPrimeNumbersToCalculate 10000
+
+namespace calculate {
+    class calcPrimeNumbers {
+    private:
+        int KnownPrimeNumbers[AmountOfPrimeNumbersToCalculate] = {2};
+    public:
+        calcPrimeNumbers() {}
+
+        bool possiblePrimeNumber(int number, int divide) {
+            int temp = number % divide;
+            if (temp == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        void calc() {
+            int start = 3;
+            int divide = 0;
+            bool Prime = true;
+            if (AmountOfPrimeNumbersToCalculate < 1) {
+                return;
+            } else {
+                //current problem: does not find any prime numbers - infinit loop
+                for (int i = 1, k = start; i < AmountOfPrimeNumbersToCalculate; k++) {
+                    divide = k - 1;
+                    Prime = true;
+                    while (Prime == true && divide > 1) {
+                        if (possiblePrimeNumber(k, divide) == false) {
+                            Prime = false;
+                        }
+                        divide--;
+                    }
+                    if (Prime == true) {
+                        KnownPrimeNumbers[i] = k;
+                        i++;
+                    }
+                }
+            }
+        }
+
+        void printPrimeNumbers() {
+            cout << "Calculated Prime Numbers: " << endl;
+            for (int i = 0; i < AmountOfPrimeNumbersToCalculate; i++) {
+                cout << KnownPrimeNumbers[i] << endl;
+            }
+        }
+    };
+}
+
+int main() {
+    time_t start = time(0);
+    class calculate::calcPrimeNumbers newRun = calculate::calcPrimeNumbers();
+    newRun.calc();
+    newRun.printPrimeNumbers();
+    cout << "The amount of time needed to calculate " << AmountOfPrimeNumbersToCalculate << " prime numbers: " << (double)difftime(time(0), start) << " seconds" << endl;
+    return 0;
+}
